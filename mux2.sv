144ea454ffe779a2f9c5a8236d70c18ad738b658
hadarCs <hadar5264@LAPTOP-HADAR>
2024-12-05 18:08:14 +0200

    adding the initial files

---
+++ mux2.sv
@@ -0,0 +1,15 @@
+// 2->1 multiplexer template
+module mux2 (
+    input logic d0,          // Data input 0
+    input logic d1,          // Data input 1
+    input logic sel,         // Select input
+    output logic z           // Output
+);
+
+// Put your code here
+// ------------------
+
+
+// End of your code
+
+endmodule
